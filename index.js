fb563f1ea349355822521aff1c63eb3efa1b4799
Wanderson Paes <wandersonpaeswop3@gmail.com>
2021-04-11 19:02:56 -0300

    Adding nodemon package

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app = express()
 
 app.listen(3000, () => console.log('Servidor rodando na porta 3000'))
 
-app.get('/home', (req, res) => res.send('Servidor rodando, tudo ok'))
+app.get('/home', (req, res) => res.send('Servidor rodando, tudo certo!'))
